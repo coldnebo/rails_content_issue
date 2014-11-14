8d7c2b47260434fe87021c7cee45a4658e1e06f1
Larry Kyrala <larry.kyrala@gmail.com>
2014-11-14 09:24:58 -0500

    wow, I actually solved it for rails3.
    unbelievable.
    
    take that Rails core team.
    
    no, seriously, that hack requires a group hug... or at least cake...
    or maybe a lap or two in DHH's race car... ;D

--- rails3_version/app/helpers/application_helper.rb
+++ rails3_version/app/helpers/application_helper.rb
@@ -12,4 +12,17 @@ module ApplicationHelper
     #controller.isolated_render_to_string("my_javascript", layout: false).html_safe
   end
 
+  def all_content_for(name)
+    # rails3 apparently has a bunch of view_flows lying around that aren't merged
+    # probably that f-ing nested view caching
+    # anyway, this is a horrible hack, but it gets at the keys we set
+    value = ""
+    ObjectSpace.each_object(ActionView::OutputFlow){ |o|
+      if o.content.has_key?(name) 
+        value = o.content[name] unless o.content[name].blank? 
+      end
+    }
+    value
+  end
+
 end
