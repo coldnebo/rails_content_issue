39860c08431cc20a05715b00b6aa8e3624cc88f1
Larry Kyrala <larry.kyrala@gmail.com>
2014-11-13 23:15:10 -0500

    here's the basic sketch... but seems to work fine in rails 4.1.7 will check with original env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def working_helper
+    %{alert('hello');}.html_safe
+  end
+
+  def broken_helper
+    controller.render_to_string("my_javascript", layout: false).html_safe
+  end
+
 end
